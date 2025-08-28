932576faff07e3d4740deb462d16dcb6df92a28c
Razvan Bulmaga <razvan.bulmaga@hogarth.com>
2025-08-28 10:32:02 +0200

    gizra-be: Add dependency injection for current user and use statement

--- web/modules/custom/server_general/src/Plugin/EntityViewBuilder/NodeGroup.php
+++ web/modules/custom/server_general/src/Plugin/EntityViewBuilder/NodeGroup.php
@@ -7,6 +7,7 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\og\Og;
 use Drupal\og\OgAccessInterface;
+use Drupal\Core\Url;
 
 /**
  * The "Node Group" plugin.
@@ -27,11 +28,19 @@ class NodeGroup extends NodeViewBuilderAbstract {
   protected $ogAccess;
 
   /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(\Symfony\Component\DependencyInjection\ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
     $instance->ogAccess = $container->get('og.access');
+    $instance->currentUser = $container->get('current_user');
     return $instance;
   }
 
@@ -47,11 +56,8 @@ class NodeGroup extends NodeViewBuilderAbstract {
    * The render array.
    */
   public function buildFull(array $build, NodeInterface $entity) {
-    /** @var \Drupal\Core\Session\AccountInterface $current_user */
-    $current_user = \Drupal::currentUser();
-
     // Check if the user is authenticated, otherwise there's nothing to do.
-    if (!$current_user->isAuthenticated()) {
+    if (!$this->currentUser->isAuthenticated()) {
       return $build;
     }
 
@@ -64,21 +70,20 @@ class NodeGroup extends NodeViewBuilderAbstract {
     }
 
     // Check if the current user is already a member of this group.
-    $is_member = Og::isMember($entity, $current_user);
+    $is_member = Og::isMember($entity, $this->currentUser);
 
     // If the user is not a member, we can check if they are allowed to subscribe.
     if (!$is_member) {
       // Use the og.access service to check for the 'subscribe' permission.
-      // The arguments are reordered to match the older OG API.
-      $can_subscribe = $this->ogAccess->userAccess($entity, 'subscribe', $current_user);
+      $can_subscribe = $this->ogAccess->userAccess($entity, 'subscribe', $this->currentUser);
 
       // Only show the greeting if the user has permission to subscribe.
       if ($can_subscribe instanceof AccessResult && $can_subscribe->isAllowed()) {
         $group_label = $entity->label();
-        $name = $current_user->getDisplayName();
+        $name = $this->currentUser->getDisplayName();
 
         // Get the URL for subscribing to the group.
-        $subscribe_url = \Drupal\Core\Url::fromRoute('og.subscribe', [
+        $subscribe_url = Url::fromRoute('og.subscribe', [
           'entity_type_id' => 'node',
           'group' => $entity->id(),
         ])->toString();
